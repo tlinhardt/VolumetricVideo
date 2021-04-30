e7dae76176a0bcdc1a0906045f69282a42c0ed6c
Tim Linhardt <legorhin@gmail.com>
2021-04-30 14:51:18 -0500

    fixed noise layer mean

--- NoiseLayer.py
+++ NoiseLayer.py
@@ -2,19 +2,20 @@ import tensorflow as tf
 import tensorflow.math as m
 import tensorflow.keras as keras
 import tensorflow.keras.layers as layers
+from tensorflow.python.keras import backend
+from tensorflow.python.ops import array_ops
 import numpy as np
 
 class NoiseLayer(layers.Layer):
   def __init__(self,mean=0,stddev=.1,**kwargs):
     super(NoiseLayer, self).__init__(**kwargs)
-    self.Noise = layers.GaussianNoise(stddev)
     self.mean = mean
-  def call(self,inputs):
-    mn = tf.ones(inputs.shape) * self.mean
-    x = m.scalar_mul(0,inputs)
-    x = m.add(self.Noise(x),mn)
-    return x
-    
-
-
-
+    self.stddev=stddev
+  def call(self,inputs,training=None):
+    noise = lambda: backend.random_normal( 
+      shape=array_ops.shape(inputs),
+      mean=self.mean,
+      stddev=self.stddev,
+      dtype=inputs.dtype
+    )
+    return backend.in_train_phase(noise, inputs, training=training)
