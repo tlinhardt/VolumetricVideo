6d3f2a72a2ad4f502b8421c7cd7a7cd5137e55c2
mkrao2021 <83318539+mkrao2021@users.noreply.github.com>
2021-05-09 14:10:06 -0500

    Update Discriminator.py

--- Discriminator.py
+++ Discriminator.py
@@ -42,3 +42,29 @@ class discriminator(k.Model):
   
 
     return x
+  
+def discriminator_loss(real_output, fake_output):
+    real_loss = cross_entropy(tf.ones_like(real_output), real_output)
+    fake_loss = cross_entropy(tf.zeros_like(fake_output), fake_output)
+    total_loss = real_loss + fake_loss
+    return total_loss
+
+class GAN(k.Model):
+    def __init__(self,discriminator,generator):
+      self.discriminator = discriminator
+      self.generator = generator 
+      self.append = layers.Concatenate(axis=1)
+
+
+    def call(self,inputs):
+      self.discriminator.trainable = False
+      x = self.generator(inputs)
+      x = self.append([inputs,x])
+      return self.discriminator(x)
+    
+def train(GAN, discriminator,epochs,data,batchsize):
+ x,y = data
+ z = np.ones(x.shape[1])
+ for iter in range(epochs):
+   GAN.fit(x,z,epochs=1,batch_size = batchsize,shuffle=True)
+   discriminator.fit(y,z,epochs=1,batch_size=batchsize,shuffle=True)
